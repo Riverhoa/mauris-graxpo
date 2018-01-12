54d6104e5f01b3e9e74d8b4426bb23810a346e60
Hoa Dang <danghoa4smilelovely@gmail.com>
2018-01-12 17:38:52 +0700

    almost done

---
+++ js/graxpo.js
@@ -0,0 +1,39 @@
+
+
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+ 
+});
+$(document).ready(function() {
+   $grid.isotope({ filter: '*' });
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+
+/*counter*/
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+    duration: 1000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+  });  
+});
