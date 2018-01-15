37d77e31cb3e78b7229104b1908ccac9b7bf5882
Riverhoa <danghoa4smilelovely@gmail.com>
2018-01-15 08:39:33 +0700

    Add files via upload

---
+++ js/graxpo.js
@@ -0,0 +1,49 @@
+
+
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+ 
+});
+$(document).ready(function() {
+   $grid.isotope({ filter: '*' });
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+
+/*counter*/
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+    duration: 8000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+  });  
+});
+
+//mobile menu
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
